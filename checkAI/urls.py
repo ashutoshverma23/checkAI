f2b0172adcf60895f96c4bea494d93f0e5fbc2da
Ashutosh Verma <ashutoshrgnict@gmail.com>
2023-09-26 21:29:52 +0530

    creating about-us page

--- checkAI/urls.py
+++ checkAI/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from checkAI import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about-us/', views.aboutUS),
 ]
